2486602ba5f581c45133f7f8fa1f9fe11d186a7d
GTRemyLeceau <smmelton@gmail.com>
2026-09-24 04:38:56 -0400

    Add host-call activity callback to module source

--- project/lib/ModernGekko/vendor/dolphin/Source/Core/Core/PowerPC/StaticRecomp/StaticRecompModuleSource.h
+++ project/lib/ModernGekko/vendor/dolphin/Source/Core/Core/PowerPC/StaticRecomp/StaticRecompModuleSource.h
@@ -13,6 +13,7 @@ struct StaticRecompModuleSource
   using HostCall = bool (*)(CPUState*, u32, void*);
   using HostCallContains = bool (*)(u32, void*);
   using HostCallRangeContains = bool (*)(u32, u32, void*);
+  using HostCallActive = bool (*)(void*);
 
   enum class Kind
   {
@@ -43,5 +44,6 @@ struct StaticRecompModuleSource
   HostCall host_call = nullptr;
   HostCallContains host_call_contains = nullptr;
   HostCallRangeContains host_call_range_contains = nullptr;
+  HostCallActive host_call_active = nullptr;
   void* host_call_user = nullptr;
 };
